8a40c432a7b7871e35fae41b7ef285dcf8b02ecc
Kristaps Kaupe <kristaps.k@intelligentsystems.lv>
2014-07-23 18:55:42 +0300

    HHVM 3.2.0+ support.

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #! /bin/sh
 
-if [[ "$HPHP_HOME" == "" ]]; then
-    echo HPHP_HOME environment variable must be set!
-    exit 1
+if [ "`which hphpize`" != "" ]; then
+    # HHVM 3.2.0 or newer
+    hphpize
+else
+    # HHVM older than 3.2.0
+    if [ "$HPHP_HOME" == "" ]; then
+        echo HPHP_HOME environment variable must be set!
+        exit 1
+    fi
+
+    $HPHP_HOME/hphp/tools/hphpize/hphpize
 fi
 
-$HPHP_HOME/hphp/tools/hphpize/hphpize
-cmake .
-make
+cmake . && make
 
